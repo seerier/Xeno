152940dc87dfe6e0d0d59936815099dadbf3b802
seerier <gxzhao@whu.edu.cn>
2024-11-19 19:52:11 +0800

    developed BDPT

--- src/integrators/bdpt.cpp
+++ src/integrators/bdpt.cpp
@@ -3,6 +3,49 @@
 
 namespace xeno {
 
+int randomWalk(const Scene &scene, Ray &ray, Spectrum beta, float pdf, int maxDepth, TransportMode mode, Vertex *path) {
+    if (maxDepth <= 0) return 0;
+    int bounces = 0;
+    float pdfFwd = pdf, pdfRev = 0;
+    while (true) {
+        Interaction intr;
+        float ray_t;
+        bool intersected = scene.intersect(ray, ray_t, intr);
+        Vertex &vertex = path[bounces], prev = path[bounces - 1];
+        if (!intersected) break;
+        vertex.setSurface(intr, beta, pdfFwd, prev);
+
+        if (++bounces >= maxDepth) break;
+
+        Vector3f wi, wo = intr.wo;
+        BSDF bsdf = intr.getBSDF();
+        //float samplePdf;
+        BxDFType sampledType;
+        //Spectrum f = bsdf.sample_f(wo, random2D(), &wi, &samplePdf, &sampledType, BxDFType::ALL, mode);
+        Spectrum f = bsdf.sample_f(wo, random2D(), &wi, &pdfFwd, &sampledType, BxDFType::ALL, mode);
+
+        if (f == Spectrum(0.f) || pdfFwd == 0.f) break;
+        beta *= f * absDot(wi, intr.n) / pdfFwd;
+        pdfRev = bsdf.pdf(wi, wo, mode);
+
+        if (sampledType | BxDFType::SPECULAR) {
+            vertex.delta = true;
+            pdfRev = pdfFwd = 0;
+        }
+
+        ray = intr.spawnRay(wi);
+        prev.pdfRev = vertex.convertDensity(pdfRev, prev);
+    }
+    return bounces;
+}
+
+int generateCameraPath(const Scene &scene, Sensor &sensor, int i, int j, int maxDepth, Vertex *path) {
+    Ray ray = sensor.generateRay(i, j, random2D());
+    path[0] = Vertex(Interaction(), Spectrum(1.f));
+    path[0].intr.p = ray.o;
+    return randomWalk(scene, ray, Spectrum(1.f), 1.f, maxDepth - 1, TransportMode::Camera, path + 1);
+}
+
 Spectrum BDPT::connectBDPT(const Scene &scene) const {
     return Spectrum(.7f);
 }
--- src/integrators/bdpt.h
+++ src/integrators/bdpt.h
@@ -4,6 +4,39 @@
 
 namespace xeno {
 
+struct Vertex {
+    Vertex() {}
+    Vertex(const Interaction &i, const Spectrum &throughput) :intr(i), beta(throughput) {}
+
+    Point3f p() const{
+        return intr.p;
+    }
+
+    Normal3f ng() const {
+        return intr.n;
+    }
+
+    float convertDensity(float pdf, const Vertex &next) const {
+        // Return solid angle density if _next_ is an infinite area light
+        Vector3f w = next.p() - p();
+        if (w.lengthSquared() == 0) return 0;
+        float invDist2 = 1 / w.lengthSquared();
+        pdf *= absDot(next.ng(), w * std::sqrt(invDist2));
+        return pdf * invDist2;
+    }
+
+    void setSurface(const Interaction &i, const Spectrum &throughput, float pdf, const Vertex &prev) {
+        intr = i;
+        beta = throughput;
+        pdfFwd = prev.convertDensity(pdf, *this);
+    }
+
+    Interaction intr;
+    Spectrum beta;
+    float pdfFwd = 0, pdfRev = 0;
+    bool delta = false;
+};
+
 class BDPT : public Integrator {
 public:
     BDPT(int i = 16, int maxDepth = 10) :spp(i), maxDepth(maxDepth) {}
